73eb8f577383dec19262faeec6c70f3ba505ba3f
lovepreet <klovepreet12199@gmail.com>
2023-10-08 23:24:12 -0400

    Almost there !! now I have the contact section in portfolio

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import Header from "./Components/Header";
 import BioPage from "./Components/BioPage";
 import Projects from "./Components/Projects";
 import Contact from "./Components/Contact";
+import { AlertProvider } from "./Context/alertContext";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
-    <>
     <ChakraProvider>
+      <AlertProvider>
     <main>
       <Header />
       <BioPage />
       <Projects/>
       <Contact/>
+      <Alert/>
+      <Footer/>
     </main>
+    </AlertProvider>
     </ChakraProvider>
-    </>
+    
   );
 }
 
